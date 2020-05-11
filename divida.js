d9714d16e9b2d66a481d3a00ea5da8d879158b0f
Luana Cherri <luana_cherri@hotmail.com>
2020-05-11 17:30:18 -0300

    feito desafio completo

---
+++ divida.js
@@ -0,0 +1,20 @@
+
+//var totalDivida = 1400; 
+
+
+//function atualizarDivida(valorRecebido) { 
+//totalDivida -= valorRecebido; 
+//return totalDivida; 
+//} 
+
+//for (var i = 0; i < 12; i++) { 
+//atualizarDivida(70); 
+//} 
+//console.log(totalDivida);
+
+var verdadeiro = true; 
+if (verdadeiro) 
+verdadeiro = false; 
+else 
+verdadeiro = !verdadeiro; 
+console.log(7 + 2 + '9'); 
